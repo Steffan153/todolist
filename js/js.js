99aff7c20ee4e86cbc645320eff73c20b6701456
Caleb Miller <40404519+Steffan153@users.noreply.github.com>
2018-07-03 11:25:07 -0600

    Update js.js

--- js/js.js
+++ js/js.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var taskToAdd = $('#task').val();
     var tasks     = $('#tasks');
     var html = '';
-    html += '<li><input type="checkbox" onclick="toggleDone($(this).next(\'.task\'), $(this))"> &nbsp; ';
+    html += '<li><input type="checkbox" onclick="toggleDone($(\'.task\').eq($(this).parent().index()), $(this))"> &nbsp; ';
     html += '<span class="task">' + taskToAdd + '</span>';
     html += ' &nbsp; &nbsp; <a href="javascript:void(0);" onclick="$(this).parent().hide()"><span class="fa fa-trash"></span></a>';
     html += ' &nbsp; <a href="javascript:void(0);" onclick="$(\'.task\').eq($(this).parent().index()).html(prompt(\'What is the new name?\'));"><span class="material-icons">mode_edit</span></a></li>';
