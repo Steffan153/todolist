e52104001f937d7c49fda0223bfc4ff6ecf02af5
Caleb Miller <40404519+Steffan153@users.noreply.github.com>
2018-07-02 18:50:26 -0600

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $('#add').click(function() {
     var taskToAdd = $('#task').val();
     var tasks     = $('#tasks');
-    $('#tasks').append('<li>' + taskToAdd + '</li>');
+    $('#tasks').append('<li>' + taskToAdd + '</li> &nbsp; &nbsp; <a href="javascript:void(0);" onclick="$(this).parent().hide()">&#xe020;</a>');
   });
 });
