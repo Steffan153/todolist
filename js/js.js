2d060d3cd48640920a339e2e001bb73410f77783
Caleb Miller <40404519+Steffan153@users.noreply.github.com>
2018-07-02 19:11:11 -0600

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $('#add').click(function() {
     var taskToAdd = $('#task').val();
     var tasks     = $('#tasks');
-    $('#tasks').append('<li><span class="task">' + taskToAdd + '</span> &nbsp; &nbsp; <a href="javascript:void(0);" onclick="$(this).parent().hide()"><span class="fa fa-trash"></span></a><a href="javascript:void(0);" onclick="$(this).prev(\'.task=\').html(prompt(\'What is the new name?\'));"><span class="mode_edit"></span></a></li>');
+    $('#tasks').append('<li><span class="task">' + taskToAdd + '</span> &nbsp; &nbsp; <a href="javascript:void(0);" onclick="$(this).parent().hide()"><span class="fa fa-trash"></span></a><a href="javascript:void(0);" onclick="$(this).prev(\'.task=\').html(prompt(\'What is the new name?\'));"><span class="material_icons">mode_edit</span></a></li>');
   });
 });
