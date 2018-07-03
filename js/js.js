8d67948a22c5302ee1fc60b3446ba061706a856c
Caleb Miller <40404519+Steffan153@users.noreply.github.com>
2018-07-02 18:56:49 -0600

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $('#add').click(function() {
     var taskToAdd = $('#task').val();
     var tasks     = $('#tasks');
-    $('#tasks').append('<li>' + taskToAdd + ' &nbsp; &nbsp; <a href="javascript:void(0);" onclick="$(this).parent().hide()">&#xe020;</a></li>');
+    $('#tasks').append('<li>' + taskToAdd + ' &nbsp; &nbsp; <a href="javascript:void(0);" onclick="$(this).parent().hide()"><span class="fa fa-trash"></span></a></li>');
   });
 });
